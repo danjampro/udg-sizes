82f24bf003b93a5a8df3a854d6cfeea7c903b42f
Dan <danjampro@sky.com>
2021-01-20 11:16:46 +1100

    plot

--- scripts/plots/model_likelihood.py
+++ scripts/plots/model_likelihood.py
@@ -3,11 +3,15 @@ import numpy as np
 from scipy import stats
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from scipy.ndimage.filters import gaussian_filter
 
 from udgsizes.core import get_config
 from udgsizes.fitting.grid import ParameterGrid
+
+
+CONFLINEKWARGS = {"linewidth": 1.3, "color": "springgreen", "linestyle": "--"}
 
 
 def likelihood_quantile(values, q):
@@ -20,6 +24,13 @@ def likelihood_quantile(values, q):
     total = csum[-1]
     idx = np.argmin(abs(csum - q*total))
     return values_sorted[idx]
+
+
+def plot_ext(ax, alpha=0.2):
+    ax.axhspan(4.4-0.19, 4.4+0.19, color="r", alpha=alpha,
+               label="van der Burg +16 (clusters), Amorisco +16")
+    ax.axhspan(3.71-0.33, 3.71+0.33, color="b", alpha=alpha,
+               label="van der Burg +17 (groups)")
 
 
 def contour_plot(ax, df, smooth=False):
@@ -123,7 +134,7 @@ def contour_plot_gauss(ax, df, fontsize=15):
     ax.legend(loc="best")
 
 
-def contour_plot_threshold(ax, df, legend=True):
+def contour_plot_threshold(ax, df, legend=True, fontsize=15):
 
     xkey = "rec_phys_alpha"
     ykey = "uae_phys_k"
@@ -156,23 +167,19 @@ def contour_plot_threshold(ax, df, legend=True):
 
     xrange = 0, 1.2
     yrange = 2.4, 5.2
-    fontsize = 15
 
-    ax.plot(xrange, [4.4, 4.4], 'r--')
-    ax.plot(xrange, [3.71, 3.71], 'b--')
-    ax.axhspan(4.4-0.19, 4.4+0.19, color="r", alpha=0.1,
-               label="van der Burg +16 (clusters), Amorisco +16")
-    ax.axhspan(3.71-0.33, 3.71+0.33, color="b", alpha=0.1,
-               label="van der Burg +17 (groups)")
+    # ax.plot(xrange, [4.4, 4.4], 'r--')
+    # ax.plot(xrange, [3.71, 3.71], 'b--')
+    plot_ext(ax)
 
     bounds = np.array([0, 1, 2, 3, 4, 5, 6])
     cmap = plt.cm.binary
     norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-    axb = inset_axes(ax, width="45%", height="3%", loc=1, borderpad=0.8)
+    axb = inset_axes(ax, width="55%", height="3%", loc=1, borderpad=0.8)
     ticks = bounds+0.5
     cb = mpl.colorbar.ColorbarBase(axb, cmap=cmap, norm=norm, spacing='proportional', ticks=ticks,
                                    boundaries=bounds, format='%1i', orientation='horizontal')
-    cb.ax.set_xticklabels(labels)
+    cb.ax.set_xticklabels(labels, fontsize=fontsize-4)
 
     ax.set_xlim(*xrange)
     ax.set_ylim(*yrange)
@@ -180,16 +187,19 @@ def contour_plot_threshold(ax, df, legend=True):
     ax.set_xlabel("$k$", fontsize=fontsize)
     ax.set_ylabel(r"$\alpha$", fontsize=fontsize)
     if legend:
-        ax.legend(loc="lower left", frameon=False)
+        ax.legend(loc="lower left", frameon=False, fontsize=fontsize-4)
 
 
-def marginal_likelihood_plot(ax, df, key, range, bins=10, fontsize=15, legend=True):
+def marginal_likelihood_plot(ax, df, key, range, bins=10, fontsize=15, legend=False,
+                             orientation="vertical", label_axes=False):
     """
     """
     values = df[key].values
     weights = np.exp(df["poisson_likelihood_2d"].values + 1000)
     ax.hist(values, range=range, bins=bins, weights=weights, density=True, histtype="step",
-            color="k", label="marginal PDF")
+            color="k", label="marginal PDF", orientation=orientation)
+    ax.hist(values, range=range, bins=bins, weights=weights, density=True, alpha=0.1,
+            color="k", label="marginal PDF", orientation=orientation)
 
     cond = (values >= range[0]) & (values < range[1])
     values = values[cond]
@@ -199,20 +209,36 @@ def marginal_likelihood_plot(ax, df, key, range, bins=10, fontsize=15, legend=Tr
     variance = np.average((values-mean)**2, weights=weights)
     std = np.sqrt(variance)
 
-    ylim = ax.get_ylim()
-    ax.plot([mean-std, mean-std], ylim, 'k--', linewidth=1.3)
-    ax.plot([mean+std, mean+std], ylim, 'k--', linewidth=1.3)
-    ax.set_ylim(ylim)
+    if orientation == "vertical":
+        ylim = ax.get_ylim()
+        ax.plot([mean-std, mean-std], ylim, **CONFLINEKWARGS)
+        ax.plot([mean+std, mean+std], ylim, **CONFLINEKWARGS)
+        ax.set_ylim(ylim)
+    else:
+        xlim = ax.get_xlim()
+        ax.plot(xlim, [mean-std, mean-std], **CONFLINEKWARGS)
+        ax.plot(xlim, [mean+std, mean+std], **CONFLINEKWARGS)
+        ax.set_xlim(xlim)
 
     if key == "rec_phys_alpha":
-        ax.axvspan(4.4-0.19, 4.4+0.19, color="b", alpha=0.2)
-        ax.axvspan(3.71-0.33, 3.71+0.33, color="r", alpha=0.2)
-        ax.set_xlabel(r"$\alpha$", fontsize=fontsize)
+        plot_ext(ax)
+
+    if label_axes:
+        if key == "rec_phys_alpha":
+            ax.set_xlabel(r"$\alpha$", fontsize=fontsize)
+        else:
+            ax.set_xlabel("$k$", fontsize=fontsize)
+    if orientation == "vertical":
+        ax.set_ylabel("PDF", fontsize=fontsize)
     else:
-        ax.set_xlabel("$k$", fontsize=fontsize)
+        ax.set_xlabel("PDF", fontsize=fontsize)
+        ax.xaxis.set_label_position("top")
+        ax.xaxis.tick_top()
 
     if legend:
         ax.legend(loc="best")
+
+    return mean, std
 
 
 if __name__ == "__main__":
@@ -229,28 +255,37 @@ if __name__ == "__main__":
     grid = ParameterGrid(model_name)
     df = grid.load_metrics()
 
-    fig = plt.figure(figsize=(12, 4))
+    x = np.random.rand(50)
+    y = np.random.rand(50)
 
-    #spec = gridspec.GridSpec(ncols=3, nrows=1, figure=fig)
-    #ax0 = fig.add_subplot(spec[:, 0])
-    #ax1 = fig.add_subplot(spec[:, 1])
-    #ax2 = fig.add_subplot(spec[:, 2])
+    fig = plt.figure(figsize=(7, 7))
+    spec = GridSpec(ncols=4, nrows=4, figure=fig)
+    ax0 = fig.add_subplot(spec[1:4, 0:3])
+    ax1 = fig.add_subplot(spec[0, 0:3])
+    ax2 = fig.add_subplot(spec[1:4, 3])
 
-    fig, ax = plt.subplots()
-    contour_plot_threshold(ax, df)
-
-    """
-    fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(12, 4))
-
-    #contour_plot_gauss(ax0, df)
     contour_plot_threshold(ax0, df)
-    marginal_likelihood_plot(ax1, df, "rec_phys_alpha", range=(3, 4.5))
-    marginal_likelihood_plot(ax2, df, "uae_phys_k", range=(0.25, 1))
+    m1, s1 = marginal_likelihood_plot(ax1, df, "uae_phys_k", range=(0.25, 1))
+    m2, s2 = marginal_likelihood_plot(ax2, df, "rec_phys_alpha", range=(3, 4.5),
+                                      orientation="horizontal")
 
-    for ax in (ax0, ax1, ax2):
-        asp = np.diff(ax.get_xlim())[0] / np.diff(ax.get_ylim())[0]
-        ax.set_aspect(asp)
-    """
+    xlim = ax0.get_xlim()
+    ylim = ax0.get_ylim()
+    ax0.plot([m1-s1, m1-s1], ylim, **CONFLINEKWARGS)
+    ax0.plot([m1+s1, m1+s1], ylim, **CONFLINEKWARGS)
+    ax0.plot(xlim, [m2-s2, m2-s2], **CONFLINEKWARGS)
+    ax0.plot(xlim, [m2+s2, m2+s2], **CONFLINEKWARGS)
+    ax0.set_xlim(xlim)
+    ax0.set_ylim(ylim)
+
+    # Turn off tick labels on marginals
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.setp(ax2.get_yticklabels(), visible=False)
+
+    # Consistent axis ranges
+    ax1.set_xlim(ax0.get_xlim())
+    ax2.set_ylim(ax0.get_ylim())
+
+    plt.savefig(image_filename, dpi=150, bbox_inches="tight")
 
     plt.show(block=False)
-    plt.savefig(image_filename, dpi=150, bbox_inches="tight")
