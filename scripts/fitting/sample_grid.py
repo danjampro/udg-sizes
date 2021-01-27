cad446757bdf733f7640da98a0b765ed6f5f03f8
Dan <danjampro@sky.com>
2021-01-27 13:46:36 +1100

    interpgrid config

--- scripts/fitting/sample_grid.py
+++ scripts/fitting/sample_grid.py
@@ -1,20 +1,18 @@
 import numpy as np
 
-from udgsizes.fitting.utils import plotting
-from udgsizes.fitting.grid import ParameterGrid
+# from udgsizes.fitting.grid import ParameterGrid
+from udgsizes.fitting.interpgrid import InterpolatedParameterGrid
 
 if __name__ == "__main__":
 
-    model_name = "blue_baldry"  # TODO: Move to command line arg
-    makeplots = False
+    # TODO: Move to command line arg
+    model_names = "blue_baldry", "blue_baldry_dwarf", "blue_baldry_trunc"
     metrics_ignore = ["kstest_2d"]  # Takes too long for whole grid
 
-    p = ParameterGrid(model_name)
+    for model_name in model_names:
 
-    with np.seterr(divide='ignore'):  # Silence annoying warnings
-        p.sample(overwrite=True)
-    dfm = p.evaluate(metrics_ignore=metrics_ignore)
+        p = InterpolatedParameterGrid(model_name)
 
-    if makeplots:
-        p.summary_plot()
-        plotting.likelihood_threshold_plot_2d(df=dfm, xkey="rec_phys_alpha", ykey="logmstar_a")
+        with np.seterr(divide='ignore'):  # Silence annoying warnings
+            p.sample(overwrite=True)
+        p.evaluate(metrics_ignore=metrics_ignore)
