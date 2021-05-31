68265ead3915f0f280b144300ed2e3e342d5910c
Dan <danjampro@sky.com>
2021-05-31 10:22:43 +1000

    faux

--- scripts/fitting/evaluate_faux_metrics.py
+++ scripts/fitting/evaluate_faux_metrics.py
@@ -1,13 +1,13 @@
 """
 Calculate metrics using model samples generated from best-fit model in place of real observations.
 """
+import argparse
 import os
 
 from udgsizes.core import get_config
 from udgsizes.fitting.grid import ParameterGrid
 
 CONFIG = get_config()
-MODEL_NAME = "blue_sedgwick_shen_0.35"
 METRICS_IGNORE = ["kstest_2d"]
 NITERS = 100
 NPROC = 4
@@ -15,9 +15,14 @@ NPROC = 4
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+    parser.add_arg("model_name", type="str", help="The model name.")
+
+    parsed_args = parser.parse_args()
+    model_name = parsed_args.model_name
+
     # Load best sample
-    grid = ParameterGrid(MODEL_NAME)
-    df = grid.load_sample(178)
+    grid = ParameterGrid(model_name)
 
     # Make directory for output
     directory = os.path.join(grid.directory, "faux")
@@ -28,7 +33,7 @@ if __name__ == "__main__":
 
         grid.logger.debug(f"Iteration {i+1} of {NITERS}.")
 
-        dff = grid.make_faux_observations(df=df)
+        dff = grid.make_faux_observations()
 
         filename = os.path.join(directory, f"metrics_{i}.csv")
 
